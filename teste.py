de1ebd4d1618ad38ae38505664caae0c0110759d
mateushlsilva <mateushls01@gmail.com>
2024-04-13 19:46:46 -0300

    feat: update status

--- teste.py
+++ teste.py
@@ -1,76 +1,73 @@
-import tensorflow as tf
-import numpy as np
 import cv2
+import mediapipe as mp
 import requests
 
-# Carregar o modelo pré-treinado
-model = tf.keras.models.load_model('modelo')
+# Initialize MediaPipe Pose detection
+mp_pose = mp.solutions.pose
 
-# Definir o tamanho esperado da entrada do modelo
-input_shape = model.input_shape[1:3]
-print("Tamanho esperado da entrada do modelo:", input_shape)
-
-# Inicializar a câmera
+# Open webcam
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Erro ao abrir a câmera!")
-    exit()
+line_left = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2 - 200)
+line_right = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2 + 200)
 
-threshold = 0.5  # Limiar de confiança
-previous_value = 0  # Armazenar o valor anterior para comparação
 
-print("Iniciando loop principal...")
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Falha ao capturar imagem da câmera.")
-        break
+state = None
+# Initialize MediaPipe Pose detector
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose_detector:
+    while cap.isOpened():
+        # Read frame from webcam
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Desenhar as linhas de divisão no frame original
-    height, width = frame.shape[:2]
-    cv2.line(frame, (width//3, 0), (width//3, height), (255, 0, 0), 2)  # Linha da zona de entrada
-    cv2.line(frame, (2*width//3, 0), (2*width//3, height), (0, 255, 0), 2)  # Linha da zona de saída
+        # Convert the frame to RGB
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Redimensionar o frame para o tamanho esperado pelo modelo para processamento
-    frame_resized = cv2.resize(frame, input_shape)
-    image = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
-    image_input = np.expand_dims(image, axis=0)
+        # Process the frame with MediaPipe
+        results = pose_detector.process(rgb_frame)
+        height, width, _ = frame.shape
+        #cx, cy = int(pose_landmark.x * width), int(pose_landmark.y * height)
+        # Check if any poses are detected
+        if results.pose_landmarks:
+            for pose_landmark in results.pose_landmarks.landmark:
+                # Obtém as coordenadas (x, y) do landmark
+                height, width, _ = frame.shape
+                cx, cy = int(pose_landmark.x * width), int(pose_landmark.y * height)
 
-    # Executar a detecção de objetos
-    detections = model.predict(image_input)
-    print("Detections:", detections)  # Imprimir detecções para verificar
+                if cx < line_left:
+                    new_state = 'Entrou'
+                    occurrence = 1
+                elif cx > line_right:
+                    new_state = 'Saiu'
+                    occurrence = 0
+                else:
+                    new_state = None
+                    occurrence = None
 
-    # Processar cada detecção
-    for detection in detections:
-        print("oi")
-        confidence, value = detection
-        if confidence > threshold:  # Verificar se a pontuação de confiança é alta o suficiente
-            print("Object detected with high confidence:", confidence)
-            print(value > previous_value)
-            print(value < previous_value)
-            if value > previous_value:
-                print("Entrada detectada")
-                try:
-                    resp = requests.post("http://localhost:8080/record", json={"occurrence": "1", "room": "Laboratorio"})
-                    print(resp)
-                except Exception as e:
-                    print(f"Não foi {e}")
-            elif value < previous_value:
-                print("Saída detectada")
-                try:
-                    resp = requests.post("http://localhost:8080/record", json={"occurrence": "0", "room": "Laboratorio"})
-                    print(resp)
-                except Exception as e:
-                    print(f"Não foi {e}")
-            previous_value = value  # Atualizar o valor anterior para comparações futuras
+                # Verifica se houve uma mudança de estado
+                if new_state and new_state != state:
+                    print(new_state)
+                    state = new_state
 
-    # Exibir o frame capturado para verificar se a câmera está funcionando
-    cv2.imshow('Camera Output', frame)
+                    # Envie a solicitação HTTP com base no estado
+                    try:
+                        resp = requests.post("http://localhost:8080/record", json={"occurrence": occurrence, "room": "Laboratorio"})
+                        print(resp)
+                    except Exception as e:
+                        print(f"Erro ao enviar a solicitação HTTP: {e}")
+            # Render detected landmarks on the frame
+            mp.solutions.drawing_utils.draw_landmarks(
+                frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-    # Verificar se a tecla 'q' foi pressionada para sair do loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the frame
+        cv2.line(frame, (line_left, 0), (line_left, height), (0, 255, 0), 2)
+        cv2.line(frame, (line_right, 0), (line_right, height), (0, 0, 255), 2)
+        cv2.imshow('Pose Detection', frame)
 
-# Liberar a câmera e fechar todas as janelas abertas
+        # Check for exit key
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Release the webcam and close all windows
 cap.release()
 cv2.destroyAllWindows()
