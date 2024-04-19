9402027de22df635a031d5ff1a3ae5ad6d7b64e5
mateushlsilva <mateushls01@gmail.com>
2024-04-18 21:58:24 -0300

    adding class DeepLearning

---
+++ ia-camera.py
@@ -0,0 +1,17 @@
+from DeepLearning import *
+
+#modelo = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8.tar.gz'
+
+modelo = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz"
+
+classFile = "coco.names"
+imagePath = "/home/mateus/Imagens/p.jpeg"
+threshold = 0.5
+
+
+detector = Deeplearning()
+detector.readClasses(classFile)
+
+detector.downloadModel(modelo)
+detector.loadModel()
+detector.predictImage(imagePath, threshold)
