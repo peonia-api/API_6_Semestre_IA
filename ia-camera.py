61f1c9cc6163192ddba6136b259d676cb9db003d
mateushlsilva <mateushls01@gmail.com>
2024-04-19 21:29:49 -0300

    adding predict by video

--- ia-camera.py
+++ ia-camera.py
@@ -2,10 +2,14 @@ from DeepLearning import *
 
 #modelo = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8.tar.gz'
 
-modelo = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz"
+#modelo = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz"
+
+modelo = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
 
 classFile = "coco.names"
 imagePath = "/home/mateus/Imagens/ps.webp"
+videoPath = '/home/mateus/Vídeos/p01.mp4'
+
 threshold = 0.5
 
 
@@ -14,4 +18,5 @@ detector.readClasses(classFile)
 
 detector.downloadModel(modelo)
 detector.loadModel()
-detector.predictImage(imagePath, threshold)
+#detector.predictImage(imagePath, threshold)
+detector.predictVideo(videoPath, threshold)
